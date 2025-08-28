aee7a03dc73b8f2611ffc0bed66da18b4b67539d
Niels Bunkenburg <nbu@informatik.uni-kiel.de>
2025-08-28 10:57:59 +0200

    Tidy test cases

--- tests/Main.hs
+++ tests/Main.hs
@@ -22,40 +22,40 @@ main = do
 progs :: [(String, String, [Result], String)]
 progs =
     [-- ND
-      ("Prelude.curry", "0 ? 1 :: Int", [RLit (Intc 0), RLit (Intc 1)], "")
-    , ("Prelude.curry", "let x = 0 ? 1 in x + x :: Int", [RLit (Intc 0), RLit (Intc 2)], "")
-    , ("Prelude.curry", "let x = 0 ? 1 :: Int in (x + (0 ? 1)) + (x + (0 ? 1))", [RLit (Intc 0), RLit (Intc 1), RLit (Intc 1), RLit (Intc 2), RLit (Intc 2), RLit (Intc 3), RLit (Intc 3), RLit (Intc 4)], "")
-    , ("PermSort.curry", "sort [5,4,3,2,1::Int]", [RCons ("Prelude", ":") [RLit (Intc 1), RCons ("Prelude", ":") [RLit (Intc 2), RCons ("Prelude", ":") [RLit (Intc 3), RCons ("Prelude", ":") [RLit (Intc 4), RCons ("Prelude", ":") [RLit (Intc 5), RCons ("Prelude", "[]") []]]]]]], "")
+      ("Prelude.curry", "0 ? 1 :: Int", [int 0, int 1], "")
+    , ("Prelude.curry", "let x = 0 ? 1 in x + x :: Int", [int 0, int 2], "")
+    , ("Prelude.curry", "let x = 0 ? 1 :: Int in (x + (0 ? 1)) + (x + (0 ? 1))", [int 0, int 1, int 1, int 2, int 2, int 3, int 3, int 4], "")
+    , ("PermSort.curry", "sort [5,4,3,2,1::Int]", [toList [int 1, int 2, int 3, int 4, int 5]], "")
 
     -- Pattern matching & function application
-    , ("Prelude.curry", "head [1::Int]", [RLit (Intc 1)], "")
+    , ("Prelude.curry", "head [1::Int]", [int 1], "")
     , ("Prelude.curry", "x && x where x free", [false, true], "")
     , ("Peano.curry", "add O O", [RCons ("Peano", "O") []], "")
     , ("Peano.curry", "add (S O) O", [RCons ("Peano", "S") [RCons ("Peano", "O") []]], "")
-    , ("Peano.curry", "p2i (add (S (S O)) (S (S O)))", [RLit (Intc 4)], "")
-    , ("Peano.curry", "p2i (Peano.pred (i2p 4))", [RLit (Intc 3)], "")
-    , ("Peano.curry", "p2i (mult (S (S O)) (S (S O)))", [RLit (Intc 4)], "")
+    , ("Peano.curry", "p2i (add (S (S O)) (S (S O)))", [int 4], "")
+    , ("Peano.curry", "p2i (Peano.pred (i2p 4))", [int 3], "")
+    , ("Peano.curry", "p2i (mult (S (S O)) (S (S O)))", [int 4], "")
     , ("Peano.curry", "p2i (fac (i2p 4))", [RLit (Intc 24)], "")
     , ("Arith.curry", "fac 5", [RLit (Intc 120)], "")
     , ("Binary.curry", "run", [RCons ("Binary", "O") [RCons ("Binary", "O") [RCons ("Binary", "O") [RCons ("Binary", "I") [RCons ("Binary", "IHi") []]]]]], "")
-    , ("Prelude.curry", "let f x = x + 1 in f 1 :: Int", [RLit (Intc 2)], "")
-    
+    , ("Prelude.curry", "let f x = x + 1 in f 1 :: Int", [int 2], "")
+
     -- Laziness
-    , ("Prelude.curry", "const (1 :: Int) (2 ? 3 :: Int)", [RLit (Intc 1)], "")
-    , ("Prelude.curry", "head [1 :: Int, 2 ? 3]", [RLit (Intc 1)], "")   
-    , ("Prelude.curry", "head [(1 :: Int)..]", [RLit (Intc 1)], "")   
-    
+    , ("Prelude.curry", "const (1 :: Int) (2 ? 3 :: Int)", [int 1], "")
+    , ("Prelude.curry", "head [1 :: Int, 2 ? 3]", [int 1], "")
+    , ("Prelude.curry", "head [(1 :: Int)..]", [int 1], "")
+
     -- Free variables
     , ("Peano.curry", "sub (S (S O)) (S O)", [RCons ("Peano", "S") [RCons ("Peano", "O") []]], "")
-    , ("Uni.curry", "last [1..8::Int]", [RLit (Intc 8)], "")
-    , ("Uni.curry", "(sort intMerge [3,2,1] xs, xs) where xs free", [RCons ("Prelude", "(,)") [true, RCons ("Prelude", ":") [RLit (Intc 1), RCons ("Prelude", ":") [RLit (Intc 2), RCons ("Prelude", ":") [RLit (Intc 3), RCons ("Prelude", "[]") []]]]]], "")
+    , ("Uni.curry", "last [1..8::Int]", [int 8], "")
+    , ("Uni.curry", "(sort intMerge [3,2,1] xs, xs) where xs free", [pair true (toList [int 1, int 2, int 3])], "")
 
     -- IO
     , ("Prelude.curry", "putStr \"Hello, World!\"", [unit], "Hello, World!")
     , ("Prelude.curry", "putChar 'x'", [unit], "x")
     , ("Prelude.curry", "putChar 'a' >> putChar 'b'", [unit], "ab")
     , ("Prelude.curry", "return 42 :: IO Int", [RLit (Intc 42)], "")
-    , ("Prelude.curry", "return (2 * 2) :: IO Int", [RLit (Intc 4)], "")
+    , ("Prelude.curry", "return (2 * 2) :: IO Int", [int 4], "")
     , ("Prelude.curry", "return [(1 :: Int)..] >>= const (return True) :: IO Bool", [true], "")
     , ("Arith.Curry", "return loop >> return True :: IO Bool", [true], "")
     , ("Prelude.curry", "let io = putStrLn \"don't share me\" in io >> io", [unit], "don't share me\ndon't share me\n")
@@ -89,34 +89,47 @@ progs =
     , ("SharingSemantics.curry", "let x = coin in const True (let y = x in y || y)", [true], "")
     , ("SharingSemantics.curry", "let x = coin in let y = coin in x || (y || (x || y))", [true, true, false], "")
     , ("SharingSemantics.curry", "let x = coin in x || (let y = coin in y || (x || y))", [true, true, false], "")
-    , ("SharingSemantics.curry", "recList (True : False : [])", [RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]], "")
-    , ("SharingSemantics.curry", "let x = coin in let y = coin in x : y : x : y : []", [RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]]], "")
-    , ("SharingSemantics.curry", "dupRT (\\_ -> coin)", [RCons ("Prelude", "(,)") [true, true], RCons ("Prelude", "(,)") [true, false], RCons ("Prelude", "(,)") [false, true], RCons ("Prelude", "(,)") [false, false]], "")
-    , ("SharingSemantics.curry", "let x = coin in dupRT (\\_ -> x)", [RCons ("Prelude", "(,)") [true, true], RCons ("Prelude", "(,)") [false, false]], "")
-    , ("SharingSemantics.curry", "let x = coin in dupShare x", [RCons ("Prelude", "(,)") [true, true], RCons ("Prelude", "(,)") [false, false]], "")
-    , ("SharingSemantics.curry", "let x = (failed :: Bool) in dupRT (\\_ -> const True x)", [RCons ("Prelude", "(,)") [true, true]], "")
-    , ("SharingSemantics.curry", "dupRT (\\_ -> (fst (coin, failed)))", [RCons ("Prelude", "(,)") [true, true], RCons ("Prelude", "(,)") [true, false], RCons ("Prelude", "(,)") [false, true], RCons ("Prelude", "(,)") [false, false]], "")
-    , ("SharingSemantics.curry", "dupShare (fst (coin, failed))", [RCons ("Prelude", "(,)") [true, true], RCons ("Prelude", "(,)") [false, false]], "")
-    , ("SharingSemantics.curry", "coini + coini", [RLit (Intc 0), RLit (Intc 1), RLit (Intc 1), RLit (Intc 2)], "")
-    , ("SharingSemantics.curry", "let x = coini in x + x", [RLit (Intc 0), RLit (Intc 2)], "")
-    , ("SharingSemantics.curry", "let x = coini in (x + coini) + (x + coini)", [RLit (Intc 0), RLit (Intc 1), RLit (Intc 1), RLit (Intc 2), RLit (Intc 2), RLit (Intc 3), RLit (Intc 3), RLit (Intc 4)], "")
-    , ("SharingSemantics.curry", "let y = (let x = coini in x + x) in y + y", [RLit (Intc 0), RLit (Intc 4)], "")
-    , ("SharingSemantics.curry", "let y = (let x = coini in x + x) in let z = coini in y + z", [RLit (Intc 0), RLit (Intc 1), RLit (Intc 2), RLit (Intc 3)], "")
-    , ("SharingSemantics.curry", "let x = 0 in x + (let y = (x + coini) in y + (let z = (y + coini) in z + 1))", [RLit (Intc 1), RLit (Intc 2), RLit (Intc 3), RLit (Intc 4)], "")
-    , ("SharingSemantics.curry", "let fxs = [coini] in (head fxs) + (head fxs)", [RLit (Intc 0), RLit (Intc 2)], "")
-    , ("SharingSemantics.curry", "let x = coin in [head (x : failed), head (x : failed)]", [RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]], "")
-    , ("SharingSemantics.curry", "let x = coin in [x, x]", [RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]], "")
-    , ("SharingSemantics.curry", "let x = coin in let y = coin in [x, y, x, y]", [RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]]], "")
-    , ("SharingSemantics.curry", "let x = (True ? False) ? (True ? False) in (x,x)", [RCons ("Prelude", "(,)") [true, true], RCons ("Prelude", "(,)") [false, false], RCons ("Prelude", "(,)") [true, true], RCons ("Prelude", "(,)") [false, false]], "")
-    , ("SharingSemantics.curry", "let x = True ? (False ? True); y = True ? False in [x, y, x, y]", [RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]]], "")
-    , ("SharingSemantics.curry", "let x = [True ? False] in (x,x)", [RCons ("Prelude", "(,)") [RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []], RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]], RCons ("Prelude", "(,)") [RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []], RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]], "")
-    , ("SharingSemantics.curry", "let y = let x = coin in x || x in (y,y)", [RCons ("Prelude", "(,)") [true, true], RCons ("Prelude", "(,)") [false, false]], "")
-    , ("SharingSemantics.curry", "let ys = let xs = [coin, coin] in xs ++ xs in (ys,ys)", [RCons ("Prelude", "(,)") [RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]]], RCons ("Prelude", "(,)") [RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]]], RCons ("Prelude", "(,)") [RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]]], RCons ("Prelude", "(,)") [RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]]]], "")
-    , ("SharingSemantics.curry", "let ys = let xs = [coin] in xs ++ xs in ys ++ ys", [RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]]], "")
-    , ("SharingSemantics.curry", "let ys = let xs = [coin] in xs ++ xs; x = coin; y = coin in x : y : ys", [RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [true, RCons ("Prelude", ":") [true, RCons ("Prelude", "[]") []]]]], RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", ":") [false, RCons ("Prelude", "[]") []]]]]], "")
+    , ("SharingSemantics.curry", "recList (True : False : [])", [toList [false, false], toList [false, true], toList [false, true]], "")
+    , ("SharingSemantics.curry", "let x = coin in let y = coin in x : y : x : y : []", [toList [true, true, true, true], toList [true, false, true, false], toList [false, true, false, true], toList [false, false, false, false]], "")
+    , ("SharingSemantics.curry", "dupRT (\\_ -> coin)", [pair true true, pair true false, pair false true, pair false false], "")
+    , ("SharingSemantics.curry", "let x = coin in dupRT (\\_ -> x)", [pair true true, pair false false], "")
+    , ("SharingSemantics.curry", "let x = coin in dupShare x", [pair true true, pair false false], "")
+    , ("SharingSemantics.curry", "let x = (failed :: Bool) in dupRT (\\_ -> const True x)", [pair true true], "")
+    , ("SharingSemantics.curry", "dupRT (\\_ -> (fst (coin, failed)))", [pair true true, pair true false, pair false true, pair false false], "")
+    , ("SharingSemantics.curry", "dupShare (fst (coin, failed))", [pair true true, pair false false], "")
+    , ("SharingSemantics.curry", "coini + coini", [int 0, int 1, int 1, int 2], "")
+    , ("SharingSemantics.curry", "let x = coini in x + x", [int 0, int 2], "")
+    , ("SharingSemantics.curry", "let x = coini in (x + coini) + (x + coini)", [int 0, int 1, int 1, int 2, int 2, int 3, int 3, int 4], "")
+    , ("SharingSemantics.curry", "let y = (let x = coini in x + x) in y + y", [int 0, int 4], "")
+    , ("SharingSemantics.curry", "let y = (let x = coini in x + x) in let z = coini in y + z", [int 0, int 1, int 2, int 3], "")
+    , ("SharingSemantics.curry", "let x = 0 in x + (let y = (x + coini) in y + (let z = (y + coini) in z + 1))", [int 1, int 2, int 3, int 4], "")
+    , ("SharingSemantics.curry", "let fxs = [coini] in (head fxs) + (head fxs)", [int 0, int 2], "")
+    , ("SharingSemantics.curry", "let x = coin in [head (x : failed), head (x : failed)]", [toList [true, true], toList [false, false]], "")
+    , ("SharingSemantics.curry", "let x = coin in [x, x]", [toList [true, true], toList [false, false]], "")
+    , ("SharingSemantics.curry", "let x = coin in let y = coin in [x, y, x, y]", [toList [true, true, true, true], toList [true, false, true, false], toList [false, true, false, true], toList [false, false, false, false]], "")
+    , ("SharingSemantics.curry", "let x = (True ? False) ? (True ? False) in (x,x)", [pair true true, pair false false, pair true true, pair false false], "")
+    , ("SharingSemantics.curry", "let x = True ? (False ? True); y = True ? False in [x, y, x, y]", [toList [true, true, true, true], toList [true, false, true, false], toList [false, true, false, true], toList [false, false, false, false], toList [true, true, true, true], toList [true, false, true, false]], "")
+    , ("SharingSemantics.curry", "let x = [True ? False] in (x,x)", [pair (toList [true]) (toList [true]), pair (toList [false]) (toList [false])], "")
+    , ("SharingSemantics.curry", "let y = let x = coin in x || x in (y,y)", [pair true true, pair false false], "")
+    , ("SharingSemantics.curry", "let ys = let xs = [coin, coin] in xs ++ xs in (ys,ys)", [pair (toList [true, true, true, true]) (toList [true, true, true, true]), pair (toList [true, false, true, false]) (toList [true, false, true, false]), pair (toList [false, true, false, true]) (toList [false, true, false, true]), pair (toList [false, false, false, false]) (toList [false, false, false, false])], "")
+    , ("SharingSemantics.curry", "let ys = let xs = [coin] in xs ++ xs in ys ++ ys", [toList [true, true, true, true], toList [false, false, false, false]], "")
+    , ("SharingSemantics.curry", "let ys = let xs = [coin] in xs ++ xs; x = coin; y = coin in x : y : ys", [toList [true, true, true, true], toList [true, true, false, false], toList [true, false, true, true], toList [true, false, false, false], toList [false, true, true, true], toList [false, true, false, false], toList [false, false, true, true], toList [false, false, false, false]], "")
     ]
 
-unit, true, false :: Result
+int :: Integer -> Result
+int x = RLit (Intc x)
+
+unit, true, false, nil :: Result
 unit = RCons ("Prelude", "()") []
 true = RCons ("Prelude", "True") []
 false = RCons ("Prelude", "False") []
+nil = RCons ("Prelude", "[]") []
+
+cons :: Result -> Result -> Result
+cons h t = RCons ("Prelude", ":") [h, t]
+
+toList :: [Result] -> Result
+toList = foldr cons nil
+
+pair :: Result -> Result -> Result
+pair l r = RCons ("Prelude", "(,)") [l, r]
